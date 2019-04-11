835e3858e303f862bcc51dba0449731d04834361
Zorrino <pierrekouyoumdjian@hotmail.fr>
2019-04-11 11:50:04 +0200

    Branche objet !

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function initialisation(){
+/* function initialisation(){
 	
 	var tableau = document.createElement("table");
 	document.body.appendChild(tableau);
@@ -7,26 +7,46 @@ function initialisation(){
 	for(i = 0; i < 6; i++){
 		var tr = document.createElement("tr");
 		tr.id = "ligne" + nb_ligne;
-		nb_ligne += 1;
 		tableau.appendChild(tr);
 		var nb_colonne = 0;
 		for(j = 0; j < 7; j++){
 			var td = document.createElement("td");
-			td.id = "colonne" + nb_colonne;
+			td.id = "" + nb_ligne + nb_colonne;
 			td.setAttribute("onClick","ajout_jeton()");
 			nb_colonne += 1;
 			tr.appendChild(td);
 			
 		}
+		nb_ligne += 1;
+	}
+} */
+function Grille(){
+	
+	this.tout = [];
+	for(i = 0; i < 6; i++){
+		this.tout.push([]);
+		for(j = 0; j < 7; j++){
+			this.tout[i].push(new Bloc(i,j));
+			console.log("Ajouter")
+		}
 	}
 }
+function Bloc(i, j) {
+	this.i = i;
+	this.j = j;
+	this.cell =function(){
+		console.log(this.i, this.j);
+	}
+}
+tableau = new Grille();
+
+function cell(i,j){
+	
+}
+
 
 function ajout_jeton(){
 	var jeton = document.createTextNode("X");
-	console.log(jeton);
-	var test = document.getElementById("colonne1");
+	var test = document.getElementById(""+0+0);
 	test.textContent=jeton.data;
-	
-	
-	
 }
